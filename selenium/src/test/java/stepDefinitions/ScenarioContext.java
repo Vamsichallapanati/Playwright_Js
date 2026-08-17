46213cb8b7cdf62aafbd092466c2bc9f47700156
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-17 17:15:01 +0530

    [AUTO] [GEN-1] [WI:3614] Generated automation scripts

---
+++ selenium/src/test/java/stepDefinitions/ScenarioContext.java
@@ -0,0 +1,17 @@
+package stepDefinitions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScenarioContext {
+    private final Map<String, Object> values = new HashMap<>();
+
+    public void set(String key, Object value) {
+        values.put(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(String key) {
+        return (T) values.get(key);
+    }
+}
