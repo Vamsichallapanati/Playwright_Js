7bbe78d61fbcd1b8b00d7b4aaf9d6b6f81969466
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-20 12:52:26 +0530

    Generated API Test Automation scripts for selected test cases

--- src/test/java/tests/login/VerifyThatLoginAPIReturnsSuccessResponseValid_Api2832Test.java
+++ src/test/java/tests/login/VerifyThatLoginAPIReturnsSuccessResponseValid_Api2832Test.java
@@ -27,10 +27,10 @@ public class VerifyThatLoginAPIReturnsSuccessResponseValid_Api2832Test {
                 ;
 
         Response response = request.when()
-                .get("/users");
+                .get("/use");
 
         QentrixReport.capture(response);
-        Assert.assertEquals(response.statusCode(), 200, "Unexpected response status code");
+        Assert.assertEquals(response.statusCode(), 202, "Unexpected response status code");
         Assert.assertNotNull(response.jsonPath().get("id"), "Expected JSON path to exist: id");
     }
 }
