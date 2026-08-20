2c02c86eab4eaa11e97266c3044b1a81092bb512
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-20 11:50:41 +0530

    Generated API Test Automation scripts for selected test cases

---
+++ src/test/java/tests/login/VerifyThatLoginAPIRejectsRequestMissingUsername_Api2834Test.java
@@ -0,0 +1,36 @@
+package tests.login;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import restUtils.reporting.Setup;
+
+import tests.QentrixConfig;
+import tests.QentrixReport;
+import tests.QentrixTestData;
+
+import static io.restassured.RestAssured.given;
+
+@Listeners(Setup.class)
+public class VerifyThatLoginAPIRejectsRequestMissingUsername_Api2834Test {
+
+    @Test(description = "Verify that login API rejects request with missing username")
+    public void verifyThatLoginAPIRejectsRequestWithMissingUsername() {
+        RestAssured.baseURI = QentrixConfig.get("base.url");
+
+        RequestSpecification request = QentrixConfig.applyAuth(given())
+                .headers(QentrixConfig.authHeaders())
+                .queryParams(QentrixConfig.authQueryParams())
+                ;
+
+        Response response = request.when()
+                .post("/login");
+
+        QentrixReport.capture(response);
+        Assert.assertEquals(response.statusCode(), 202, "Unexpected response status code");
+        Assert.assertNotNull(response.jsonPath().get("id"), "Expected JSON path to exist: id");
+    }
+}
