46d832030ef57f1890e2aa1a0531c3c3e2d75a61
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-20 13:00:37 +0530

    bznkzk

---
+++ src/test/java/tests/login/VerifyThatLoginAPIReturnsSuccessResponseValid_Api2832Test.java
@@ -0,0 +1,36 @@
+package tests.login;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import restUtils.reporting.Setup;
+
+import tests.QentrixConfig;
+import tests.QentrixReport;
+import tests.QentrixTestData;
+
+import static io.restassured.RestAssured.given;
+
+@Listeners(Setup.class)
+public class VerifyThatLoginAPIReturnsSuccessResponseValid_Api2832Test {
+
+    @Test(description = "Verify that login API returns success response with valid credentials")
+    public void verifyThatLoginAPIReturnsSuccessResponseWithValidCredentials() {
+        RestAssured.baseURI = QentrixConfig.get("base.url");
+
+        RequestSpecification request = QentrixConfig.applyAuth(given())
+                .headers(QentrixConfig.authHeaders())
+                .queryParams(QentrixConfig.authQueryParams())
+                ;
+
+        Response response = request.when()
+                .get("/users");
+
+        QentrixReport.capture(response);
+        Assert.assertEquals(response.statusCode(), 202, "Unexpected response status code");
+        Assert.assertFalse(response.getBody().asString().isBlank(), "Response body should not be empty");
+    }
+}
