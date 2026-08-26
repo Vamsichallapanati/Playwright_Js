ccd5fa450629d76c278bcf90716483af96ee5f36
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-26 16:09:06 +0530

    Generated API Test Automation scripts for selected test cases

---
+++ src/test/resources/karate-config.js
@@ -0,0 +1,13 @@
+function fn() {
+  var config = {
+    baseUrl: 'https://www.demoblaze.com/',
+    authType: 'none',
+    token: '',
+    username: '',
+    password: '',
+    apiKeyName: '',
+    apiKeyValue: '',
+    apiKeyLocation: ''
+  };
+  return config;
+}
