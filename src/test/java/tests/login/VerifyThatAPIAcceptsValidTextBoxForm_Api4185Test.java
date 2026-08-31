c0fbd4e19e6d5113e03fa39cdab0dde094b37c80
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-31 12:50:49 +0530

    Generated API Test Automation scripts for selected test cases

---
+++ src/test/java/tests/login/VerifyThatAPIAcceptsValidTextBoxForm_Api4185Test.java
@@ -0,0 +1,36 @@
+package tests.login;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import restUtils.reporting.Setup;
+
+import tests.QentrixConfig;
+import tests.QentrixReport;
+import tests.QentrixTestData;
+
+import static io.restassured.RestAssured.given;
+
+@Listeners(Setup.class)
+public class VerifyThatAPIAcceptsValidTextBoxForm_Api4185Test {
+
+    @Test(description = "Verify that API accepts valid text box form submission request with complete data")
+    public void verifyThatAPIAcceptsValidTextBoxFormSubmissionRequestWithCompleteData() {
+        RestAssured.baseURI = QentrixConfig.get("base.url");
+
+        RequestSpecification request = QentrixConfig.applyAuth(given())
+                .headers(QentrixConfig.authHeaders())
+                .queryParams(QentrixConfig.authQueryParams())
+                ;
+
+        Response response = request.when()
+                .patch("/user/");
+
+        QentrixReport.capture(response);
+        Assert.assertEquals(response.statusCode(), 204, "Unexpected response status code");
+        Assert.assertEquals(String.valueOf(response.jsonPath().get("id")), "", "Unexpected JSON value for id");
+    }
+}
