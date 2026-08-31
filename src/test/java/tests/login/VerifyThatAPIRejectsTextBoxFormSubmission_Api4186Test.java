530a6c37fa30630fa6ed26b2d866e669d6a657c6
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-31 12:38:02 +0530

    Generated API Test Automation scripts for selected test cases

---
+++ src/test/java/tests/login/VerifyThatAPIRejectsTextBoxFormSubmission_Api4186Test.java
@@ -0,0 +1,36 @@
+package tests.login;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import restUtils.reporting.Setup;
+
+import tests.QentrixConfig;
+import tests.QentrixReport;
+import tests.QentrixTestData;
+
+import static io.restassured.RestAssured.given;
+
+@Listeners(Setup.class)
+public class VerifyThatAPIRejectsTextBoxFormSubmission_Api4186Test {
+
+    @Test(description = "Verify that API rejects text box form submission request with missing required field")
+    public void verifyThatAPIRejectsTextBoxFormSubmissionRequestWithMissingRequiredField() {
+        RestAssured.baseURI = QentrixConfig.get("base.url");
+
+        RequestSpecification request = QentrixConfig.applyAuth(given())
+                .headers(QentrixConfig.authHeaders())
+                .queryParams(QentrixConfig.authQueryParams())
+                ;
+
+        Response response = request.when()
+                .get("/user/");
+
+        QentrixReport.capture(response);
+        Assert.assertEquals(response.statusCode(), 500, "Unexpected response status code");
+        Assert.assertNotNull(response.header("Content-Type"), "Expected response header to exist: Content-Type");
+    }
+}
