57a0626b11a66789d6db9c2ec515cec793daad9d
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-08-31 12:38:03 +0530

    Generated API Test Automation scripts for selected test cases

---
+++ src/test/java/tests/login/VerifyThatAPIReturnsForbiddenStatusUserApproval_Api4160Test.java
@@ -0,0 +1,36 @@
+package tests.login;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import restUtils.reporting.Setup;
+
+import tests.QentrixConfig;
+import tests.QentrixReport;
+import tests.QentrixTestData;
+
+import static io.restassured.RestAssured.given;
+
+@Listeners(Setup.class)
+public class VerifyThatAPIReturnsForbiddenStatusUserApproval_Api4160Test {
+
+    @Test(description = "Verify that API returns forbidden status for user without approval permissions")
+    public void verifyThatAPIReturnsForbiddenStatusForUserWithoutApprovalPermissions() {
+        RestAssured.baseURI = QentrixConfig.get("base.url");
+
+        RequestSpecification request = QentrixConfig.applyAuth(given())
+                .headers(QentrixConfig.authHeaders())
+                .queryParams(QentrixConfig.authQueryParams())
+                ;
+
+        Response response = request.when()
+                .patch("/user/");
+
+        QentrixReport.capture(response);
+        Assert.assertEquals(response.statusCode(), 400, "Unexpected response status code");
+        Assert.assertNotNull(response.jsonPath().get("id"), "Expected JSON path to exist: id");
+    }
+}
