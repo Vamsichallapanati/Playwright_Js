f0a7c2d6246be2836c2bb160863cfd81174353e9
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-09-02 15:29:41 +0530

    [AUTO] [GEN-1] [WI:selected] Generated automation scripts

--- updatedplaywrightjshybrid/fixtures/test.js
+++ updatedplaywrightjshybrid/fixtures/test.js
@@ -1,5 +1,11 @@
 import { test as base, expect } from '@playwright/test';
+import ClaimsApprovalPage from '../pages/ClaimsApprovalPage.js';
 
-export const test = base.extend({});
+export const test = base.extend({
+  claimsApprovalPage: async ({ page }, use) => {
+    await use(new ClaimsApprovalPage(page));
+  }
+});
 
 export { expect };
+
