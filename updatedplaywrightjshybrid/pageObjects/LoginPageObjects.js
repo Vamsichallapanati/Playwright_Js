98360519fac24f48ee4077efb8092e4f58f4ac9f
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-09-02 10:30:51 +0530

    [AUTO] [GEN-1] [WI:selected] Generated automation scripts

---
+++ updatedplaywrightjshybrid/pageObjects/LoginPageObjects.js
@@ -0,0 +1,9 @@
+export class LoginPageObjects {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.locator('#user-name');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('#login-button');
+  }
+}
+
