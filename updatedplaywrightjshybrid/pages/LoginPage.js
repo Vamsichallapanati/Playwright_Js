977a9350a067b7b0d4a5a6797d7b48d341a9d00e
Vamsichallapanati <102377117+Vamsichallapanati@users.noreply.github.com>
2026-09-08 11:43:11 +0530

    [AUTO] [GEN-1] [WI:selected] Generated automation scripts

--- updatedplaywrightjshybrid/pages/LoginPage.js
+++ updatedplaywrightjshybrid/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import BasePage from './BasePage.js';
 import { LoginPageObjects } from '../pageObjects/LoginPageObjects.js';
+import { credentials } from '../config/environment.js';
 
 export default class LoginPage extends BasePage {
   constructor(page) {
@@ -7,42 +8,20 @@ export default class LoginPage extends BasePage {
     this.locators = new LoginPageObjects(page);
   }
 
-  async navigateToLoginPage(url) {
-    await this.page.goto(url);
-    await this.page.waitForLoadState('domcontentloaded');
-  }
-
-  async leaveUsernameEmptyAndEnterPassword(password) {
-    await this.fill(this.locators.usernameInput, '');
+  async login(username, password) {
+    await this.fill(this.locators.usernameInput, username);
     await this.fill(this.locators.passwordInput, password);
-  }
-
-  async clickLoginButton() {
     await this.click(this.locators.loginButton);
   }
 
-  async verifyValidationErrorDisplayed() {
-    await this.expectVisible(this.locators.validationError);
+  async loginWithDefaultCredentials() {
+    const creds = credentials();
+    await this.login(creds.username, creds.password);
   }
 
-  async verifyUserRemainsOnLoginPage() {
-    await this.expectVisible(this.locators.loginForm);
-    await this.expectPath(/login/);
-  }
-
-  async verifyPasswordFieldContainsValue() {
-    await this.page.waitForTimeout(500);
-    const passwordValue = await this.locators.passwordInput.inputValue();
-    if (!passwordValue) {
-      throw new Error('Password field should contain value');
-    }
-  }
-
-  async verifyUsernameFieldIsEmpty() {
-    const usernameValue = await this.locators.usernameInput.inputValue();
-    if (usernameValue !== '') {
-      throw new Error('Username field should remain empty');
-    }
+  async expectAuthenticated() {
+    await this.page.waitForLoadState('networkidle');
+    await this.expectPath(/inventory/);
   }
 }
 
